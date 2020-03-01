0e05442d420e2549203c5cde4f3f9d6761c2b976
mark chaitin <mchaitin@gmail.com>
2020-02-29 18:02:31 -0800

    added skeleton contact page

--- src/screens/Contact.jsx
+++ src/screens/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Paper } from '@material-ui/core';
 import translateString from '../utils/StringHelper';
 import useTitle from '../hooks/useTitle';
 
@@ -8,7 +9,26 @@ const Contact = () => {
 
   return (
     <div>
-      Stuff
+      <Paper style={{ padding: 25 }}>
+        <h1>
+          Contact us
+        </h1>
+        <h2>
+          Email: rosecitychoralcollective@gmail.com
+        </h2>
+        <h2>
+          Facebook:
+          {' '}
+          <a
+            href="https://www.facebook.com/rosecitychoralcollective"
+          >
+              @rosecitychoralcollective
+          </a>
+        </h2>
+        <h2>
+          Phone: 503-867-5309. Ask for Jenny.
+        </h2>
+      </Paper>
     </div>
   );
 };
