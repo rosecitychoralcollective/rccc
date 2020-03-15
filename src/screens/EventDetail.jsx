0e28c72d946f5c3b2ee29500ff6e74dd329f1504
mark chaitin <mchaitin@gmail.com>
2020-03-15 15:51:55 -0700

    added images to event detail pages. added some stock photos

--- src/screens/EventDetail.jsx
+++ src/screens/EventDetail.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { getEventById } from '../resources/events';
+import cat from '../resources/assets/cat.jpg';
 import useTitle from '../hooks/useTitle';
 
 const EventDetail = ({ id }) => {
@@ -8,6 +9,7 @@ const EventDetail = ({ id }) => {
 
   return (
     <div style={{ padding: 75, maxWidth: 700 }}>
+      <img src={cat} alt={event.title} />
       <h1>{event.title}</h1>
       <p>{event.description}</p>
     </div>
