4bfebd8df38287e71a067122bf4c8b5897554103
mark chaitin <mchaitin@gmail.com>
2020-06-18 22:56:34 -0700

    Updates

---
+++ precache-manifest.12e732d08d51549364f4753d9e823171.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd01263fd69983290cf3a0d9ee7f5a0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57b4fc8951ac03ce7cdc",
+    "url": "/static/css/main.dc6ae21c.chunk.css"
+  },
+  {
+    "revision": "3b597630eb18110f781a",
+    "url": "/static/js/2.a4dc156f.chunk.js"
+  },
+  {
+    "revision": "57b4fc8951ac03ce7cdc",
+    "url": "/static/js/main.2d608f02.chunk.js"
+  },
+  {
+    "revision": "05b094a78669c2ab4096",
+    "url": "/static/js/runtime-main.e9d196e7.js"
+  },
+  {
+    "revision": "ff431c0a94ca0e3104c9279b7facee8e",
+    "url": "/static/media/Annie.ff431c0a.jpg"
+  },
+  {
+    "revision": "f93f2f2fba558aa03c819c9c46e54244",
+    "url": "/static/media/Molly.f93f2f2f.jpg"
+  },
+  {
+    "revision": "6e22b353d22bd4c4b16e593e454672c4",
+    "url": "/static/media/Performance.6e22b353.jpg"
+  },
+  {
+    "revision": "5f3bf7306c45df074430c364afd9a092",
+    "url": "/static/media/Zosia.5f3bf730.jpg"
+  }
+]);
